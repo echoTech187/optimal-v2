227e96e121e37914bf6ec177665dafc59edf7977
echotech187 <echotech187@gmail.com>
2025-09-23 08:52:40 +0700

    update intro content

--- src/app/components/intro/page.tsx
+++ src/app/components/intro/page.tsx
@@ -17,7 +17,7 @@ export default function Intro() {
                 <div className="relative w-full max-sm:h-[320px] max-lg:h-[480px] lg:h-[570px] max-w-[1300px] mx-auto">
                     <div className="absolute max-sm:bottom-6 max-md:bottom-12 md:bottom-20 left-1/2 transform -translate-x-1/2 my-auto w-full text-white">
                         <div className="flex flex-col justify-end gap-4 px-6 2xl:px-0 xs:w-full sm:w-[60%] md:w-[80%] lg:w-[80%] xl:w-[80%]">
-                            <h1 className="max-sm:text-xl sm:text-2xl md:text-3xl lg:text-5xl xl:text-6xl font-bold uppercase">Platform Solusi Bimbingan Belajar Terbaik dalam Genggaman Anda</h1>
+                            <h1 className="max-sm:text-xl sm:text-2xl md:text-3xl lg:text-5xl xl:text-6xl font-bold uppercase">PLATFORM SOLUSI BIMBINGAN BELAJAR UKOM TERBAIK</h1>
                             <p className="max-sm:text-xs text-sm">Tersedia Layanan Kesehatan yang Terpercaya dan Mudah Diakses untuk Kebutuhan Anda</p>
                             <Link href="#services">
                             <Icon icon="tabler:arrow-down" className="max-md:size-6 md:size-8 lg:size-12"  />
